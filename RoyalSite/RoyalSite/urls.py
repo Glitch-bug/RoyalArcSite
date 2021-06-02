bf0d761c6aa10d5bacc00467ed5dbd5b992f903e
six yrn <kwabenayiadom271@gmail.com>
2021-06-02 14:22:50 +0000

    Static Files

--- RoyalSite/RoyalSite/urls.py
+++ RoyalSite/RoyalSite/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('RoyalPages/', include('RoyalPages.urls'))
+    path('', include('RoyalPages.urls'))
 ]
