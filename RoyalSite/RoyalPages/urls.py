7e86ac175e6f00c50e2b139ad88e2508ec1cd530
six yrn <kwabenayiadom271@gmail.com>
2021-06-17 20:52:19 +0000

    style #2

--- RoyalSite/RoyalPages/urls.py
+++ RoyalSite/RoyalPages/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "RoyalPages"
 urlpatterns = [
     path("", views.home, name="home"),
-    path("<str:title>", views.blog_post, name="blog"),
-    path("/posts", views.posts, name="posts"),
-    path("/about us", views.about, name="about")
+    path("posts/<str:title>", views.blog_post, name="blog"),
+    path("posts", views.posts, name="posts"),
+    path("about us", views.about, name="about")
 ]
